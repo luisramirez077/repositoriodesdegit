600126d28eb7a65121e12f9cca9cd21df6b027d1
luis fernando <luislomejors2001@gmail.com>
2022-02-23 16:50:58 -0500

    Agregar archivos de proyecto.

---
+++ PracticaPOO703/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticaPOO703
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
